24335d7edd93419f3ac294e65eac42932ae8ab3c
LakshmiBolla <nagalakshmi.bolla@jobsnprofiles.com>
2024-02-28 20:11:22 -0800

    git commit -m "help"

--- src/main/java/helps/Help.java
+++ src/main/java/helps/Help.java
@@ -4,7 +4,9 @@ public class Help {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("help with them");
+		System.out.println("help");
+		System.out.println("test");
 	}
 
 }
